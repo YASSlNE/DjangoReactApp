4cb2de945ca778b9eb217f2d4f384ab390f98d9f
YASSlNE <mdyassinechiboub@gmail.com>
2022-06-18 20:32:55 +0100

    completeted the backend

--- todoappbackend/api/views.py
+++ todoappbackend/api/views.py
@@ -2,6 +2,56 @@ from rest_framework import generics, permissions
 from .serializers import TodoSerializer, TodoToggleCompleteSerializer
 from todo.models import Todo 
 
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+from rest_framework.parsers import JSONParser
+from rest_framework.authtoken.models import Token
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from django.contrib.auth import authenticate
+
+
+
+
+@csrf_exempt
+def signup(request):
+	if(request.method=='POST'):
+		try:
+			data = JSONParser().parse(request)
+			user=User.objects.create_user(
+				username=data['username'],
+				password=data['password'])
+			user.save()
+			
+			token=Token.objects.create(user=user)
+
+			return JsonResponse({'token':str(token)}, status=201)
+		except IntegrityError:
+			return JsonResponse({'error':'username taken. choose another username'},
+				status=400)
+
+@csrf_exempt
+def login(request):
+	if(request.method=='POST'):
+			data=JSONParser().parse(request)
+			user=authenticate(
+				request,
+				username=data['username'],
+				password=data['password'])
+			if user is None:
+				return JsonResponse(
+					{
+					'error':'unable to login.'
+					},
+					status=400)
+			else:
+				try:
+					token=Token.objects.get(user=user)
+				except:
+					token=Token.objects.create(user=user)
+				return JsonResponse({'token':str(token)}, status=201)
+
 
 class TodoListCreate(generics.ListCreateAPIView):
 	serializer_class=TodoSerializer
--- todoappbackend/backend/settings.py
+++ todoappbackend/backend/settings.py
@@ -40,7 +40,17 @@ INSTALLED_APPS = [
     'todo',
     'rest_framework',
     'api',
+    'rest_framework.authtoken',
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
